4b21a2ccaa025f5d7e67e92307d0919dd8ad65ac
Tommy <tommykindle83@gmail.com>
2019-07-17 13:31:26 -0700

    Set up files for SASS attempting to get axios working but there is a bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import axios  from "axios";
+
+axios
+.get('https://api.nasa.gov/planetary/apod?api_key=agKZPLbqC4bCnshJcQXpx1sX7oDabUcRvJN1dg5c ')
+.then(res => {
+  console.loq('Result', res)
+});
+
 
 function App() {
   return (
