4c869d45700ddb8f34c8ae752391969dbef7b11a
Tommy <tommykindle83@gmail.com>
2019-07-17 19:27:46 -0700

    Fixed indentation of code for OCD reasons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
-import axios  from "axios";
+import axios from "axios";
 import Card from "./Card"
 
 
 function App() {
- const [data,setData] =  useState({});
-  useEffect(()=>{
+  const [data, setData] = useState({});
+  useEffect(() => {
     axios
-    .get('https://api.nasa.gov/planetary/apod?api_key=agKZPLbqC4bCnshJcQXpx1sX7oDabUcRvJN1dg5c ')
-    .then(res => {
-      console.log('Result', res)
-      setData(res.data)
-    });
-},[])
+      .get('https://api.nasa.gov/planetary/apod?api_key=agKZPLbqC4bCnshJcQXpx1sX7oDabUcRvJN1dg5c ')
+      .then(res => {
+        console.log('Result', res)
+        setData(res.data)
+      });
+  }, [])
 
   return (
     <div className="App">
-      <Card title={data.title} photoCaption ={data.explanation} imgUrl={data.url} media_type={data.media_type}/>
+      <Card title={data.title} photoCaption={data.explanation} imgUrl={data.url} media_type={data.media_type} />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
--- src/Card.js
+++ src/Card.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Card = (props) => {
   return (
     <div>
-    <h1>{props.title}</h1>
-    {props.media_type === 'video' ? <iframe width="420" height="315"src ={props.imgUrl}> </iframe> : <img src= {props.imgUrl}/> }
-    <p>{props.photoCaption}</p>
-  </div>
+      <h1>{props.title}</h1>
+      {props.media_type === 'video' ? <iframe width="420" height="315" src={props.imgUrl}> </iframe> : <img src={props.imgUrl} />}
+      <p>{props.photoCaption}</p>
+    </div>
   )
 }
 
